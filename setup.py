642aa94a16bc7a0b46492a192f42d2bfc48d6426
Tim Schneider <tim.schneider@nb-dev.de>
2015-02-24 10:56:49 +0100

    Removing special pip readme... does not seem to be needed

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ logger = logging.getLogger(__name__)
 import os
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
+#with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+#    README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -28,7 +28,7 @@ setup(
     install_requires=['Django >=1.7',],
     license='MIT License',
     description='A widget for django form date fields that displays three inputs (day, month, year).',
-    long_description=README,
+    #long_description=README,
     url='http://github.com/NB-Dev/django-splitdate',
     author='Nothrbridge Development Konrad & Schneider GbR',
     author_email='mail@nb-dev.de',
